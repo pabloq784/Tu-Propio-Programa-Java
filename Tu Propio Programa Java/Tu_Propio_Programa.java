ff87ec875dbf4efd7674d188609eb1d5b0879895
pabloq784 <60245898+pabloq784@users.noreply.github.com>
2020-03-10 23:48:25 -0300

    Add files via upload

---
+++ Tu Propio Programa Java/Tu_Propio_Programa.java
@@ -0,0 +1,13 @@
+public class Tu_Propio_Programa {
+    public static void main(String[] args) {
+        String name = "Pablo";
+        String surname = "Quincha, Tengo";
+        byte age = 33;
+        String city = "Mi ciudad es Burbank, CA";
+        System.out.println("Mi Nombre es: " + name +" " + surname +" "+ age + " años de edad.");
+        System.out.println(city);
+    }
+
+}
+
+
